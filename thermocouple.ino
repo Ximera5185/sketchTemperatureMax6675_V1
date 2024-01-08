12e2938f0b909e2b24b00d6a2cfa8b6cfd0ce192
Ximera5185 <48877880+Ximera5185@users.noreply.github.com>
2024-01-08 11:59:07 +0300

    1
    1

--- thermocouple.ino
+++ thermocouple.ino
@@ -3,19 +3,22 @@
 int thermoDO = 2;  //он же SO
 int thermoCS = 3;
 int thermoCLK = 4;  //он же SCK
-
+int ledPin = 13; // переменная с пином подключенного светодиода
+int temperature;
+int criticalTemperature = 100;
 MAX6675 thermocouple(thermoCLK, thermoCS, thermoDO);
 
 int vccPin = 5;  //пин для питания
 int gndPin = 6;  //пин для земли
-
-
+String key = "term";
+String package;
 
 void setup() {
   Serial.begin(9600);
   //активируем питание и землю
   pinMode(vccPin, OUTPUT); digitalWrite(vccPin, HIGH);
   pinMode(gndPin, OUTPUT); digitalWrite(gndPin, LOW);
+  pinMode(ledPin,OUTPUT); // назначаем наш пин выходом
 
   //ждем стабилизации чипа MAX
   delay(500);
@@ -23,6 +26,26 @@ void setup() {
 
 void loop() {
   //Выводим показания в монитор порта
-    Serial.println((int)thermocouple.readCelsius());
- delay(200);
+    //Serial.println((int)thermocouple.readCelsius());
+    temperature = (int)thermocouple.readCelsius();
+    //package = key + (int)thermocouple.readCelsius();
+    package = key + temperature;
+
+    Serial.println(package);
+
+    TurnAlarm();
+
+ delay(1000); // Найти у Гайвера цикл таймера Gyver
+}
+
+void TurnAlarm()
+{
+  if(temperature >= criticalTemperature)
+  {
+    digitalWrite(ledPin,HIGH);
+  }
+  else
+  {
+    digitalWrite(ledPin,LOW);
+  }
 }
